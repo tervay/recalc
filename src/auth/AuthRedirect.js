d64b926357602664faad21c203dbd306df7740c1
Justin Tervay <justin@tervay.com>
2020-06-02 16:26:20 -0400

    login bugfix

--- src/auth/AuthRedirect.js
+++ src/auth/AuthRedirect.js
@@ -1,19 +1,8 @@
 import { auth } from "db";
 import React from "react";
 import { Redirect } from "react-router-dom";
-import { useAction } from "redux-zero/react";
 
 export default function AuthRedirect() {
-  const signIn = useAction((state, id) => ({ isSignedIn: true, id }));
-  const signOut = useAction((state) => ({ isSignedIn: false, id: null }));
-  auth.listen((user) => {
-    if (user === null) {
-      signOut();
-    } else {
-      signIn(user.localId);
-    }
-  });
-
   auth.handleSignInRedirect();
   return <Redirect to="/" />;
 }
--- src/auth/store.js
+++ src/auth/store.js
@@ -1,6 +1,23 @@
+import { auth } from "db";
 import createStore from "redux-zero";
+import { bindActions } from "redux-zero/utils";
 
 const initialState = { isSignedIn: false, id: null };
 const store = createStore(initialState);
+
+const actions = store => ({
+    signIn: (state, id) => ({ isSignedIn: true, id }),
+    signOut: (state) => ({ isSignedIn: false, id: null })
+})
+
+const boundActions = bindActions(actions, store);
+
+auth.listen((user) => {
+    if (user === null) {
+        boundActions.signOut();
+    } else {
+        boundActions.signIn(user.localId);
+    }
+});
 
 export default store;
