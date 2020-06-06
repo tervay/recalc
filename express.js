dd5e1890dd6aaf8cb889ebfc7ee9a3d43f613fe6
Justin Tervay <justin@tervay.com>
2020-06-06 16:11:46 -0400

    Swap to express server

---
+++ express.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", function (req, res) {
+  res.sendFile("index.html", { root: path.join(__dirname, "build/") });
+});
+
+app.listen(process.env.PORT || 5000);
