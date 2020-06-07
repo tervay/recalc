960e23dd07720b3ed4ddd0534e10bcc1523ab911
Justin Tervay <justin@tervay.com>
2020-06-07 14:48:35 -0400

    I broke a whole bunch. Oops

---
+++ src/db/index.js
@@ -0,0 +1,34 @@
+import Auth from 'firebase-auth-lite';
+import firebaseConfig from "db/key";
+import Firestore from 'firebase-firestore-lite'
+
+export const auth = new Auth({
+  apiKey: firebaseConfig.apiKey
+});
+
+export const db = new Firestore({ projectId: firebaseConfig.projectId, auth })
+
+export function save(user, name, url, query) {
+  const userRef = db.reference(`users/${user}`)
+  const res = userRef.get()
+    .then((r) => {
+      // User is found!
+      return r;
+    }).catch((e) => {
+      // User not found. Creating
+      userRef.set({
+        id: user,
+        configs: []
+      })
+      return userRef
+    }).then((r) => {
+      // Append new object to configs
+      const newConfigs = r.configs;
+      newConfigs.push({ name, url, query });
+      return userRef.update({
+        configs: newConfigs
+      })
+    }).then((r) => {
+      console.log('updated state: ', r);
+    })
+}
