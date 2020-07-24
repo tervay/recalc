6476eb83eaeaaadd5e2862b036a7300a2438c46b
Justin Tervay <justin@tervay.com>
2020-07-24 17:18:48 -0400

    Add local firebase key

---
+++ src/calculators/chains/chains.math.test.js
@@ -0,0 +1,14 @@
+import each from "jest-each";
+
+import { teethToPD } from "./math";
+
+each([
+  [32, "#25", 2.551],
+  [40, "#25", 3.186],
+  [66, "#25", 5.254],
+  [22, "#35", 2.635],
+  [33, "#35", 3.945],
+  [54, "#35", 6.449],
+]).test("chain teeth -> pitch diameter", (teeth, chain, expected) => {
+  expect(teethToPD(teeth, chain).to("in").scalar).toBeCloseTo(expected, 3);
+});
--- src/db/key.js
+++ src/db/key.js
@@ -1,4 +1,15 @@
-export default {
+
+let key = {
   apiKey: "AIzaSyDuLDBeMeSvH6rMb8A43m_aAssCS-wWxbY",
   projectId: "recalc-1590210745953",
 };
+
+if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
+  key = {
+    apiKey: "AIzaSyDOPkHtByQXGnNRcOuazuxjF9yW-T_-OZs",
+    projectId: "recalc-dev",
+  };
+}
+
+export default key;
+
