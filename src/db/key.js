aeed0c63306b6239530f65dad3c490a4bff7e64b
Justin Tervay <justin@tervay.com>
2020-07-24 17:41:13 -0400

    Update remaining outdated packages

--- src/db/key.js
+++ src/db/key.js
@@ -1,10 +1,12 @@
-
 let key = {
   apiKey: "AIzaSyDuLDBeMeSvH6rMb8A43m_aAssCS-wWxbY",
   projectId: "recalc-1590210745953",
 };
 
-if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
+if (
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
+) {
   key = {
     apiKey: "AIzaSyDOPkHtByQXGnNRcOuazuxjF9yW-T_-OZs",
     projectId: "recalc-dev",
@@ -12,4 +14,3 @@ if (window.location.hostname === "localhost" || window.location.hostname === "12
 }
 
 export default key;
-
