469c572bcf35ed6673c42ee23b32bafbde20799f
Justin Tervay <justin@tervay.com>
2020-08-04 05:54:10 -0400

    Add brotli+gzip compression

--- src/dev/express.js
+++ src/dev/express.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const path = require("path");
 const sslRedirect = require("heroku-ssl-redirect");
+const expressStaticGzip = require('express-static-gzip');
 
 const app = express();
 app.use(sslRedirect());
 
-app.use(express.static(path.join(__dirname, "../../build")));
+app.use(expressStaticGzip(path.join(__dirname, "../../build"), {
+  enableBrotli: true
+}));
 
 app.get("*", function (req, res) {
   res.sendFile("index.html", { root: path.join(__dirname, "build/") });
