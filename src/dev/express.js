8387be13cbfbbbe030e30b77ca7916babee95841
Justin Tervay <justin@tervay.com>
2020-08-04 06:26:39 -0400

    Prefer brotli

--- src/dev/express.js
+++ src/dev/express.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const path = require("path");
 const sslRedirect = require("heroku-ssl-redirect");
-const expressStaticGzip = require('express-static-gzip');
+const expressStaticGzip = require("express-static-gzip");
 
 const app = express();
 app.use(sslRedirect());
 
-app.use(expressStaticGzip(path.join(__dirname, "../../build"), {
-  enableBrotli: true
-}));
+app.use(
+  expressStaticGzip(path.join(__dirname, "../../build"), {
+    enableBrotli: true,
+    orderPreference: ["br", "gz"],
+  })
+);
 
 app.get("*", function (req, res) {
   res.sendFile("index.html", { root: path.join(__dirname, "build/") });
