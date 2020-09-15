8e0be0ffe9efe1140d735485a86f68da84a4f24e
Justin Tervay <justin@tervay.com>
2020-09-14 23:19:22 -0400

    Redo homepage

--- src/web/landing/Tile.js
+++ src/web/landing/Tile.js
@@ -5,30 +5,35 @@ import { Link } from "react-router-dom";
 export default function Tile(props) {
   return (
     <Link to={props.to}>
-      <div className="card">
-        <div className="card-image">
-          <figure className="image is-4by3">
-            <picture>
-              {props.image && (
-                <>
-                  <source type="image/webp" srcSet={props.image + ".webp"} />
-                  <source type="image/png" srcSet={props.image + ".png"} />
-                </>
-              )}
-              <img
-                src={
-                  (props.image ||
-                    "https://bulma.io/images/placeholders/1280x960") + ".png"
-                }
-                alt={props.title}
-              />
-            </picture>
+      <div className={"box"}>
+        <article className="media">
+          <figure className="media-left">
+            <figure className="image is-128x128">
+              <picture>
+                {props.image && (
+                  <>
+                    <source type="image/webp" srcSet={props.image + ".webp"} />
+                    <source type="image/png" srcSet={props.image + ".png"} />
+                  </>
+                )}
+                <img
+                  src={
+                    (props.image ||
+                      "https://bulma.io/images/placeholders/128x128") + ".png"
+                  }
+                  alt={props.title}
+                />
+              </picture>
+            </figure>
           </figure>
-        </div>
-        <div className="card-content">
-          <p className="title is-4">{props.title}</p>
-          <p className="subtitle is-6">{props.subtitle}</p>
-        </div>
+          <div className="media-content">
+            <div className="content">
+              <p>
+                <strong className={"title"}>{props.title}</strong>
+              </p>
+            </div>
+          </div>
+        </article>
       </div>
     </Link>
   );
