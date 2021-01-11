4e5255dc6914d073cd93ee96948e22a307cf6d04
Justin Tervay <justin@tervay.com>
2021-01-11 15:45:53 -0500

    Update filament links

--- src/common/models/Filament.js
+++ src/common/models/Filament.js
@@ -53,9 +53,9 @@ const filamentMap = keyBy(
       youngsModulus: new Measurement(2636, "MPa"),
       tensileStrength: new Measurement(46.6, "MPa"),
       bendingStrength: new Measurement(85.1, "MPa"),
-      charpy: new Measurement(2.7, "kJ/m^2"),
+      charpy: new Measurement(2.68, "kJ/m^2"),
       sources: [
-        "https://us.polymaker.com/Downloads/TDS/PolyLite_PLA_TDS_V4.pdf",
+        "https://us.polymaker.com/Downloads/TDS/PolyLite%20PLA_TDS_V5.pdf",
       ],
     },
     {
@@ -65,9 +65,9 @@ const filamentMap = keyBy(
       youngsModulus: new Measurement(1879, "MPa"),
       tensileStrength: new Measurement(28.1, "MPa"),
       bendingStrength: new Measurement(48, "MPa"),
-      charpy: new Measurement(12.2, "kJ/m^2"),
+      charpy: new Measurement(12.1, "kJ/m^2"),
       sources: [
-        "https://us.polymaker.com/Downloads/TDS/PolyMax_PLA_TDS_V4.pdf",
+        "https://us.polymaker.com/Downloads/TDS/PolyMax%20PLA_TDS_V5.pdf",
       ],
     },
     {
@@ -76,10 +76,10 @@ const filamentMap = keyBy(
       density: new Measurement(1.12, "g/cm3"),
       youngsModulus: new Measurement(2174, "MPa"),
       tensileStrength: new Measurement(33.3, "MPa"),
-      bendingStrength: new Measurement(59, "MPa"),
+      bendingStrength: new Measurement(72.8, "MPa"),
       charpy: new Measurement(12.6, "kJ/m^2"),
       sources: [
-        "https://us.polymaker.com/Downloads/TDS/PolyLite_ABS_TDS_V4.pdf",
+        "https://us.polymaker.com/Downloads/TDS/PolyLite_ABS_TDS_V5.pdf",
       ],
     },
     {
@@ -90,9 +90,7 @@ const filamentMap = keyBy(
       tensileStrength: new Measurement(43.8, "MPa"),
       bendingStrength: new Measurement(73.4, "MPa"),
       charpy: new Measurement(10.3, "kJ/m^2"),
-      sources: [
-        "https://polymaker.com/Downloads/TDS/PolyLite_ASA_TDS_V4.1.pdf",
-      ],
+      sources: ["https://polymaker.com/Downloads/TDS/PolyLite_ASA_TDS_V5.pdf"],
     },
     {
       material: "PETG",
@@ -103,7 +101,7 @@ const filamentMap = keyBy(
       bendingStrength: new Measurement(53.7, "MPa"),
       charpy: new Measurement(5.1, "kJ/m^2"),
       sources: [
-        "https://us.polymaker.com/Downloads/TDS/PolyLite_PETG_TDS_V4.pdf",
+        "https://us.polymaker.com/Downloads/TDS/PolyLite_PETG_TDS_V5.pdf",
       ],
     },
     {
@@ -115,7 +113,7 @@ const filamentMap = keyBy(
       bendingStrength: new Measurement(58.3, "MPa"),
       charpy: new Measurement(9.7, "kJ/m^2"),
       sources: [
-        "https://us.polymaker.com/Downloads/TDS/PolyMax_PETG_TDS_V4.pdf",
+        "https://us.polymaker.com/Downloads/TDS/PolyMax_PETG_TDS_V5.pdf",
       ],
     },
     {
@@ -125,9 +123,9 @@ const filamentMap = keyBy(
       youngsModulus: new Measurement(2307, "MPa"),
       tensileStrength: new Measurement(62.7, "MPa"),
       bendingStrength: new Measurement(100.4, "MPa"),
-      charpy: new Measurement(3.4, "kJ/m^2"),
+      charpy: new Measurement(3.41, "kJ/m^2"),
       sources: [
-        "https://us.polymaker.com/Downloads/TDS/PolyLite_PC_TDS_V4.pdf",
+        "https://us.polymaker.com/Downloads/TDS/PolyLite_PC_TDS_V5.pdf",
       ],
     },
     {
@@ -138,7 +136,7 @@ const filamentMap = keyBy(
       tensileStrength: new Measurement(59.7, "MPa"),
       bendingStrength: new Measurement(94.1, "MPa"),
       charpy: new Measurement(25.1, "kJ/m^2"),
-      sources: ["https://us.polymaker.com/Downloads/TDS/PolyMax_PC_TDS_V4.pdf"],
+      sources: ["https://us.polymaker.com/Downloads/TDS/PolyMax_PC_TDS_V5.pdf"],
     },
   ],
   "name"
