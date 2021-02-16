725f0b4f7aaba94d6fe8623fd54706b58f550f00
Justin Tervay <justin@tervay.com>
2021-02-16 18:03:16 -0500

    Update packages

--- src/common/services/firebase.js
+++ src/common/services/firebase.js
@@ -2,7 +2,7 @@ import "firebase/auth";
 import "firebase/firestore";
 
 import config from "common/services/gkey";
-import * as firebase from "firebase/app";
+import firebase from "firebase/app";
 
 firebase.initializeApp(config);
 
