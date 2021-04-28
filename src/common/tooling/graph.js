cc93780671bd173285579f20964eb96016ba0681
Justin Tervay <justin@tervay.com>
2021-04-28 18:46:47 -0400

    Fix import with new zoom plugin beta

--- src/common/tooling/graph.js
+++ src/common/tooling/graph.js
@@ -1,11 +1,11 @@
 import { Chart, registerables } from "chart.js";
-import * as ChartZoomPlugin from "chartjs-plugin-zoom";
+import zoomPlugin from "chartjs-plugin-zoom";
 import { ReactChart } from "chartjs-react";
 import styles from "exports.module.scss";
 
 Chart.defaults.font.family = styles.font_family;
 
-ReactChart.register(...registerables, ChartZoomPlugin.Zoom);
+ReactChart.register(...registerables, zoomPlugin);
 
 export function Graph(props) {
   return <ReactChart {...props} />;
