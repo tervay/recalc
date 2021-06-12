284b60de4b5b1e4a3743844bd90a45f932b6963a
Justin Tervay <justin@tervay.com>
2021-06-12 12:43:54 -0400

    Add some prusament filaments

--- src/common/models/tests/Filament.test.js
+++ src/common/models/tests/Filament.test.js
@@ -4,7 +4,7 @@ describe("Filament", () => {
   test("getAllFilaments returns all Filament instances", () => {
     const all = Filament.getAllFilaments();
 
-    expect(all).toHaveLength(10);
+    expect(all).toHaveLength(15);
     for (const f of all) {
       expect(f).toBeInstanceOf(Filament);
       expect(f.name).toBeDefined();
