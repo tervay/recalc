c27b9ed9e2e99c9c003ca0c9cbf6362a55e36c76
Justin Tervay <justin@tervay.com>
2022-12-24 08:37:40 -0500

    Increase default cypress timeout and increase retries to 3

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,12 +6,16 @@ export default defineConfig({
       // implement node event listeners here
     },
   },
-
   projectId: "rq9vfm",
   component: {
     devServer: {
       framework: "react",
       bundler: "vite",
     },
+  },
+  defaultCommandTimeout: 1000 * 10,
+  retries: {
+    runMode: 3,
+    openMode: 3,
   },
 });
