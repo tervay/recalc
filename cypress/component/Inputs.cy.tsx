61b1f7773da5c26eaf0a0eb54d2d839a35a604e3
Justin Tervay <justin@tervay.com>
2022-12-31 05:49:16 -0500

    Migrate from removed mountHook fn

--- cypress/component/Inputs.cy.tsx
+++ cypress/component/Inputs.cy.tsx
@@ -3,64 +3,62 @@ import L0Number from "common/components/io/new/inputs/L0/L0Number";
 import L0Select from "common/components/io/new/inputs/L0/L0Select";
 
 import { L0NumberProps } from "common/components/io/new/inputs/types/Types";
-import { mount, mountHook } from "cypress/react";
-import React, { useState } from "react";
+import { StateHook } from "common/models/ExtraTypes";
+import { mount } from "cypress/react";
+import React from "react";
 
 describe("L0 Inputs", () => {
   describe("Number", () => {
     it("Calls stateHook[1] with updated state", () => {
-      mountHook(() => useState(0)).then((hook) => {
-        const spy = cy.spy(hook.current![1]);
-        mount(
-          <L0Number
-            stateHook={[hook.current![0], spy]}
-            id="ss"
-            size={"small"}
-            rounded={false}
-            primaryIf={() => false}
-            linkIf={() => false}
-            infoIf={() => false}
-            successIf={() => false}
-            warningIf={() => false}
-            dangerIf={() => false}
-            disabledIf={() => false}
-            static={false}
-            readonly={false}
-            delay={0}
-            roundTo={0}
-          />
-        );
-
+      const hook: StateHook<number> = [0, cy.spy()];
+      mount(
+        <L0Number
+          stateHook={hook}
+          id="ss"
+          size={"small"}
+          rounded={false}
+          primaryIf={() => false}
+          linkIf={() => false}
+          infoIf={() => false}
+          successIf={() => false}
+          warningIf={() => false}
+          dangerIf={() => false}
+          disabledIf={() => false}
+          static={false}
+          readonly={false}
+          delay={0}
+          roundTo={0}
+        />
+      ).then(() => {
         cy.get("input")
           .type("1")
           .then((_) => {
-            expect(spy).to.be.calledWith(1);
+            expect(hook[1]).to.be.calledWith(1);
           });
       });
     });
 
     it("Is disabled when disabledIf is true", () => {
-      mountHook(() => useState(0)).then((hook) => {
-        mount(
-          <L0Number
-            stateHook={hook.current!}
-            id="ss"
-            size={"small"}
-            rounded={false}
-            primaryIf={() => false}
-            linkIf={() => false}
-            infoIf={() => false}
-            successIf={() => false}
-            warningIf={() => false}
-            dangerIf={() => false}
-            disabledIf={() => true}
-            static={false}
-            readonly={false}
-            delay={0}
-            roundTo={0}
-          />
-        );
-
+      const hook: StateHook<number> = [0, cy.spy()];
+      mount(
+        <L0Number
+          stateHook={hook}
+          id="ss"
+          size={"small"}
+          rounded={false}
+          primaryIf={() => false}
+          linkIf={() => false}
+          infoIf={() => false}
+          successIf={() => false}
+          warningIf={() => false}
+          dangerIf={() => false}
+          disabledIf={() => true}
+          static={false}
+          readonly={false}
+          delay={0}
+          roundTo={0}
+        />
+      ).then(() => {
         cy.get("input").then((div) => {
           expect(div).to.be.disabled;
         });
@@ -68,33 +66,31 @@ describe("L0 Inputs", () => {
     });
 
     it("Has the given ID", () => {
-      mountHook(() => useState(0)).then((hook) => {
-        mount(
-          <L0Number
-            stateHook={hook.current!}
-            id="ss"
-            size={"small"}
-            rounded={false}
-            primaryIf={() => false}
-            linkIf={() => false}
-            infoIf={() => false}
-            successIf={() => false}
-            warningIf={() => false}
-            dangerIf={() => false}
-            disabledIf={() => false}
-            static={false}
-            readonly={false}
-            delay={0}
-            roundTo={0}
-          />
-        );
-
+      const hook: StateHook<number> = [0, cy.spy()];
+      mount(
+        <L0Number
+          stateHook={hook}
+          id="ss"
+          size={"small"}
+          rounded={false}
+          primaryIf={() => false}
+          linkIf={() => false}
+          infoIf={() => false}
+          successIf={() => false}
+          warningIf={() => false}
+          dangerIf={() => false}
+          disabledIf={() => false}
+          static={false}
+          readonly={false}
+          delay={0}
+          roundTo={0}
+        />
+      ).then(() => {
         cy.get("input").then((div) => {
           expect(div).to.have.id("ss");
         });
       });
     });
-
     const makeProps: (
       stateHook: L0NumberProps["stateHook"]
     ) => L0NumberProps = (stateHook) => {
@@ -128,14 +124,10 @@ describe("L0 Inputs", () => {
       const className = "is-" + name.replace("If", "");
 
       it(`[${name}] colors`, () => {
-        mountHook(() => useState(0)).then((hook) => {
-          mount(
-            <L0Number
-              {...makeProps(hook.current!)}
-              {...{ [name]: () => true }}
-            />
-          );
-
+        const hook: StateHook<number> = [0, cy.spy()];
+        mount(
+          <L0Number {...makeProps(hook)} {...{ [name]: () => true }} />
+        ).then(() => {
           cy.get("input").then((div) => {
             expect(div).to.have.class(className);
           });
@@ -144,148 +136,139 @@ describe("L0 Inputs", () => {
     });
 
     it("Does not update state with long delay", () => {
-      mountHook(() => useState(0)).then((hook) => {
-        const spy = cy.spy(hook.current![1]);
-        mount(
-          <L0Number
-            stateHook={[hook.current![0], spy]}
-            id="ss"
-            size={"small"}
-            rounded={false}
-            primaryIf={() => false}
-            linkIf={() => false}
-            infoIf={() => false}
-            successIf={() => false}
-            warningIf={() => false}
-            dangerIf={() => false}
-            disabledIf={() => false}
-            static={false}
-            readonly={false}
-            delay={10000}
-            roundTo={0}
-          />
-        );
+      const hook: StateHook<number> = [0, cy.spy()];
+      mount(
+        <L0Number
+          stateHook={hook}
+          id="ss"
+          size={"small"}
+          rounded={false}
+          primaryIf={() => false}
+          linkIf={() => false}
+          infoIf={() => false}
+          successIf={() => false}
+          warningIf={() => false}
+          dangerIf={() => false}
+          disabledIf={() => false}
+          static={false}
+          readonly={false}
+          delay={10000}
+          roundTo={0}
+        />
+      );
 
-        cy.get("input")
-          .type("1")
-          .then((_) => {
-            expect(spy).to.not.be.called;
-          });
-      });
-    });
-
-    it("Updates state after short delay", () => {
-      mountHook(() => useState(0)).then((hook) => {
-        const spy = cy.spy(hook.current![1]);
-        mount(
-          <L0Number
-            stateHook={[hook.current![0], spy]}
-            id="ss"
-            size={"small"}
-            rounded={false}
-            primaryIf={() => false}
-            linkIf={() => false}
-            infoIf={() => false}
-            successIf={() => false}
-            warningIf={() => false}
-            dangerIf={() => false}
-            disabledIf={() => false}
-            static={false}
-            readonly={false}
-            delay={1000}
-            roundTo={0}
-          />
-        );
-
-        cy.get("input")
-          .type("123")
-          .wait(250)
-          .then((_) => {
-            expect(spy).to.not.be.called;
-          })
-          .wait(1000)
-          .then((_) => {
-            expect(spy).to.be.calledWith(123);
-          });
-      });
-    });
-  });
-
-  describe("Boolean", () => {
-    it("Should update state", () => {
-      mountHook(() => useState(false)).then((hook) => {
-        const spy = cy.spy(hook.current![1]);
-        mount(<L0Boolean id="bool" stateHook={[hook.current![0], spy]} />);
-
-        cy.get("input")
-          .click()
-          .then((div) => {
-            expect(div).to.be.checked;
-            expect(spy).to.be.called;
-          });
-      });
-    });
-
-    it("Should have the given ID", () => {
-      mountHook(() => useState(false)).then((hook) => {
-        mount(<L0Boolean id="bool" stateHook={hook.current!} />);
-
-        cy.get("input").then((div) => {
-          expect(div).to.have.id("bool");
+      cy.get("input")
+        .type("1")
+        .then((_) => {
+          expect(hook[1]).to.not.be.called;
         });
-      });
     });
   });
 
-  describe("Select", () => {
-    it("Has options from choices", () => {
-      mountHook(() => useState("Foo")).then((hook) => {
-        mount(
-          <L0Select
-            id={"select"}
-            stateHook={hook.current!}
-            size={"small"}
-            loadingIf={() => false}
-            rounded={false}
-            primaryIf={() => false}
-            linkIf={() => false}
-            infoIf={() => false}
-            successIf={() => false}
-            warningIf={() => false}
-            dangerIf={() => false}
-            choices={["Foo", "Bar", "Baz"]}
-          />
-        );
+  it("Updates state after short delay", () => {
+    const hook: StateHook<number> = [0, cy.spy()];
+    mount(
+      <L0Number
+        stateHook={hook}
+        id="ss"
+        size={"small"}
+        rounded={false}
+        primaryIf={() => false}
+        linkIf={() => false}
+        infoIf={() => false}
+        successIf={() => false}
+        warningIf={() => false}
+        dangerIf={() => false}
+        disabledIf={() => false}
+        static={false}
+        readonly={false}
+        delay={1000}
+        roundTo={0}
+      />
+    ).then(() => {
+      cy.get("input")
+        .type("123")
+        .wait(250)
+        .then((_) => {
+          expect(hook[1]).to.not.be.called;
+        })
+        .wait(1000)
+        .then((_) => {
+          expect(hook[1]).to.be.calledWith(123);
+        });
+    });
+  });
+});
 
-        cy.get("option").should("have.length", 3);
+describe("Boolean", () => {
+  it("Should update state", () => {
+    const hook: StateHook<boolean> = [false, cy.spy()];
+    mount(<L0Boolean id="bool" stateHook={hook} />).then(() => {
+      cy.get("input")
+        .click()
+        .then((div) => {
+          expect(div).to.be.checked;
+          expect(hook[1]).to.be.called;
+        });
+    });
+  });
+
+  it("Should have the given ID", () => {
+    const hook: StateHook<boolean> = [false, cy.spy()];
+    mount(<L0Boolean id="bool" stateHook={hook} />).then(() => {
+      cy.get("input").then((div) => {
+        expect(div).to.have.id("bool");
       });
     });
-    it("Should update state", () => {
-      mountHook(() => useState("Foo")).then((hook) => {
-        const spy = cy.spy(hook.current![1]);
-        mount(
-          <L0Select
-            id={"select"}
-            stateHook={[hook.current![0], spy]}
-            size={"small"}
-            loadingIf={() => false}
-            rounded={false}
-            primaryIf={() => false}
-            linkIf={() => false}
-            infoIf={() => false}
-            successIf={() => false}
-            warningIf={() => false}
-            dangerIf={() => false}
-            choices={["Foo", "Bar", "Baz"]}
-          />
-        );
+  });
+});
 
-        cy.get("select")
-          .select("Bar")
-          .then((_) => {
-            expect(spy).to.have.been.calledWith("Bar");
-          });
-      });
+describe("Select", () => {
+  it("Has options from choices", () => {
+    const hook: StateHook<string> = ["Foo", cy.spy()];
+    mount(
+      <L0Select
+        id={"select"}
+        stateHook={hook}
+        size={"small"}
+        loadingIf={() => false}
+        rounded={false}
+        primaryIf={() => false}
+        linkIf={() => false}
+        infoIf={() => false}
+        successIf={() => false}
+        warningIf={() => false}
+        dangerIf={() => false}
+        choices={["Foo", "Bar", "Baz"]}
+      />
+    ).then(() => {
+      cy.get("option").should("have.length", 3);
+    });
+  });
+
+  it("Should update state", () => {
+    const hook: StateHook<string> = ["Foo", cy.spy()];
+    mount(
+      <L0Select
+        id={"select"}
+        stateHook={hook}
+        size={"small"}
+        loadingIf={() => false}
+        rounded={false}
+        primaryIf={() => false}
+        linkIf={() => false}
+        infoIf={() => false}
+        successIf={() => false}
+        warningIf={() => false}
+        dangerIf={() => false}
+        choices={["Foo", "Bar", "Baz"]}
+      />
+    ).then(() => {
+      cy.get("select")
+        .select("Bar")
+        .then((_) => {
+          expect(hook[1]).to.have.been.calledWith("Bar");
+        });
     });
   });
 });
