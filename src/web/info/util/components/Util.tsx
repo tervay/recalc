467220286712bb566e6eafc36d2f46342124931e
Justin Tervay <justin@tervay.com>
2023-11-12 13:37:09 -0500

    Add spacer calc

--- src/web/info/util/components/Util.tsx
+++ src/web/info/util/components/Util.tsx
@@ -1,11 +1,13 @@
 import Bearings from "web/info/util/components/Bearings";
 import { HoleSizes } from "web/info/util/components/HoleSizes";
 import MountingCheatSheet from "web/info/util/components/MountingCheatSheet";
+import SpacerCalc from "web/info/util/components/Spacers";
 
 export default function Util(): JSX.Element {
   return (
     <>
       <HoleSizes />
+      <SpacerCalc />
       <MountingCheatSheet />
       <Bearings />
       {/* <ExtrusionPrices /> */}
