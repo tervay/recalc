c4eb737a748044cea885f8e6b65e7df4bc023cfc
Justin Tervay <justin@tervay.com>
2024-11-16 21:17:42 -0500

    Add playwright

---
+++ tests/home.spec.ts
@@ -0,0 +1,40 @@
+import { expect, test } from "@playwright/test";
+
+test.describe.configure({ mode: "parallel" });
+
+test.describe("Home page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("localhost:3000");
+  });
+
+  [
+    ["Belt Calculator", "/belts"],
+    ["Chain Calculator", "/chains"],
+    ["Pneumatics Calculator", "/pneumatics"],
+    ["Flywheel Calculator", "/flywheel"],
+    ["Arm Calculator", "/arm"],
+    ["Linear Mechanism Calculator", "/linear"],
+    ["Intake Calculator", "/intake"],
+  ].forEach(([title, url]) => {
+    test(`Links to ${title}`, async ({ page }) => {
+      await page.click(`text=${title}`);
+      expect(page.url()).toContain(url);
+    });
+
+    test(`${title} has a navbar that links home`, async ({ page }) => {
+      await page.click(`text=${title}`);
+      expect(page.url()).toContain(url);
+    });
+  });
+
+  [
+    ["Motor Playground", "/motors"],
+    ["Compressor Playground", "/compressors"],
+    ["About ReCalc", "/about"],
+  ].forEach(([title, url]) => {
+    test(`Links to ${title}`, async ({ page }) => {
+      await page.click(`text=${title}`);
+      expect(page.url()).toContain(url);
+    });
+  });
+});
